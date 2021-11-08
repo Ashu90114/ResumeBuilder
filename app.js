19c446371daa767bdfca5e0729131eed627b9566
Ashu <ashu90114@gmail.com>
2021-11-08 11:39:04 +0530

    fixed the issue of stylesheet not working

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var express = require("express");
-var app = express();
-var request  = require("request");
-var path = require('path');
+const express = require("express");
+const app = express();
+const request  = require("request");
+const path = require('path');
+const ejsMate = require('ejs-mate');
 
-app.set("view engine", "ejs");
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", function(req, res){
     res.render("home");
 });
